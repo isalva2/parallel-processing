6c3bb84060d04c85b6e35c431b37cf051f745e8c
Isaac Salvador <isalva2@uic.edu>
2024-03-12 14:57:58 -0500

    it's MPI time

---
+++ MPI/Hello-World-MPI.c
@@ -0,0 +1,13 @@
+// This is Hello World implemented with MPI
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    int npes, myrank;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, *npes);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+    printf("From process %d out of %d, Hellow World!\n", myrank, npes);
+    MPI_Finalize();
+    return 0;
+}
