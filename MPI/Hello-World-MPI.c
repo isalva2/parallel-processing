0a31bba5607bd7bfe0e996287f9e5a79adb63f74
Isaac Salvador <isalva2@uic.edu>
2024-03-12 16:48:51 -0500

    it's MPI time

--- MPI/Hello-World-MPI.c
+++ MPI/Hello-World-MPI.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[]) {
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, *npes);
     MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
-    printf("From process %d out of %d, Hellow World!\n", myrank, npes);
+    printf("From process %d out of %d, Hello World!\n", myrank, npes);
     MPI_Finalize();
     return 0;
 }
