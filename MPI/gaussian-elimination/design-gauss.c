b2bac3afc0744530741028943493e06ed2578b36
Isaac Salvador <isalva2@uic.edu>
2024-03-14 21:29:54 -0500

    working on gaussian elimination with MPI

--- MPI/gaussian-elimination/design-gauss.c
+++ MPI/gaussian-elimination/design-gauss.c
@@ -6,19 +6,19 @@
  * You need not submit the provided code.
  */
 
+#include <math.h>
+#include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <math.h>
-#include <sys/types.h>
-#include <sys/times.h>
 #include <sys/time.h>
+#include <sys/times.h>
+#include <sys/types.h>
 #include <time.h>
-#include <mpi.h>
+#include <unistd.h>
 
 /* Program Parameters */
 #define MAXN 20000 /* Max value of N */
-int N;            /* Matrix size */
+int N;             /* Matrix size */
 
 /* Matrices and vectors */
 volatile float A[MAXN][MAXN], B[MAXN], X[MAXN];
@@ -34,141 +34,117 @@ void gauss(); /* The function you will provide.
                */
 
 /* returns a seed for srand based on the time */
-unsigned int time_seed()
-{
-    struct timeval t;
-    struct timezone tzdummy;
+unsigned int time_seed() {
+  struct timeval t;
+  struct timezone tzdummy;
 
-    gettimeofday(&t, &tzdummy);
-    return (unsigned int)(t.tv_usec);
+  gettimeofday(&t, &tzdummy);
+  return (unsigned int)(t.tv_usec);
 }
 
-
 /* Set the program parameters from the command-line arguments */
-void parameters(int argc, char **argv)
-{
-    int seed = 0; /* Random seed */
-    char uid[32]; /*User name */
+void parameters(int argc, char **argv) {
+  int seed = 0; /* Random seed */
+  char uid[32]; /*User name */
 
-    /* Read command-line arguments */
-    srand(time_seed()); /* Randomize */
+  /* Read command-line arguments */
+  srand(time_seed()); /* Randomize */
 
-    if (argc == 3)
-    {
-        seed = atoi(argv[2]);
-        srand(seed);
-        printf("Random seed = %i\n", seed);
+  if (argc == 3) {
+    seed = atoi(argv[2]);
+    srand(seed);
+    printf("Random seed = %i\n", seed);
+  }
+  if (argc >= 2) {
+    N = atoi(argv[1]);
+    if (N < 1 || N > MAXN) {
+      printf("N = %i is out of range.\n", N);
+      exit(0);
     }
-    if (argc >= 2)
-    {
-        N = atoi(argv[1]);
-        if (N < 1 || N > MAXN)
-        {
-            printf("N = %i is out of range.\n", N);
-            exit(0);
-        }
-    }
-    else
-    {
-        printf("Usage: %s <matrix_dimension> [random seed]\n",
-               argv[0]);
-        exit(0);
-    }
+  } else {
+    printf("Usage: %s <matrix_dimension> [random seed]\n", argv[0]);
+    exit(0);
+  }
 
-    /* Print parameters */
-    printf("\nMatrix dimension N = %i.\n", N);
+  /* Print parameters */
+  printf("\nMatrix dimension N = %i.\n", N);
 }
 
 /* Initialize A and B (and X to 0.0s) */
-void initialize_inputs()
-{
-    int row, col;
+void initialize_inputs() {
+  int row, col;
 
-    printf("\nInitializing...\n");
-    for (col = 0; col < N; col++)
-    {
-        for (row = 0; row < N; row++)
-        {
-            A[row][col] = (float)rand() / 32768.0;
-        }
-        B[col] = (float)rand() / 32768.0;
-        X[col] = 0.0;
+  printf("\nInitializing...\n");
+  for (col = 0; col < N; col++) {
+    for (row = 0; row < N; row++) {
+      A[row][col] = (float)rand() / 32768.0;
     }
+    B[col] = (float)rand() / 32768.0;
+    X[col] = 0.0;
+  }
 }
 
 /* Print input matrices */
-void print_inputs()
-{
-    int row, col;
+void print_inputs() {
+  int row, col;
 
-    if (N < 10)
-    {
-        printf("\nA =\n\t");
-        for (row = 0; row < N; row++)
-        {
-            for (col = 0; col < N; col++)
-            {
-                printf("%5.2f%s", A[row][col], (col < N - 1) ? ", " : ";\n\t");
-            }
-        }
-        printf("\nB = [");
-        for (col = 0; col < N; col++)
-        {
-            printf("%5.2f%s", B[col], (col < N - 1) ? "; " : "]\n");
-        }
+  if (N < 10) {
+    printf("\nA =\n\t");
+    for (row = 0; row < N; row++) {
+      for (col = 0; col < N; col++) {
+        printf("%5.2f%s", A[row][col], (col < N - 1) ? ", " : ";\n\t");
+      }
     }
+    printf("\nB = [");
+    for (col = 0; col < N; col++) {
+      printf("%5.2f%s", B[col], (col < N - 1) ? "; " : "]\n");
+    }
+  }
 }
 
-void print_X()
-{
-    int row;
+void print_X() {
+  int row;
 
-    if (N < 100)
-    {
-        printf("\nX = [");
-        for (row = 0; row < N; row++)
-        {
-            printf("%5.2f%s", X[row], (row < N - 1) ? "; " : "]\n");
-        }
+  if (N < 100) {
+    printf("\nX = [");
+    for (row = 0; row < N; row++) {
+      printf("%5.2f%s", X[row], (row < N - 1) ? "; " : "]\n");
     }
+  }
 }
 
-int main(int argc, char *argv[])
-{
-    /* Gaussian Elimination */
-    int numprocs, myrank;
+int main(int argc, char *argv[]) {
+  // this is a test
+  /* Gaussian Elimination */
+  int numprocs, myrank;
 
-    MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
-    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+  MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
 
-    /* Let's see if these processes process lol */
-    if (myrank == 0)
-    {
-        /* Process program parameters */
-        parameters(argc, argv);
+  /* Let's see if these processes process lol */
+  if (myrank == 0) {
+    /* Process program parameters */
+    parameters(argc, argv);
 
-        /* Initialize A and B */
-        initialize_inputs();
+    /* Initialize A and B */
+    initialize_inputs();
 
-        /* Print input matrices */
-        print_inputs();
+    /* Print input matrices */
+    print_inputs();
 
-        printf("Hi, I am proc %d\n", myrank);
-    }
-    else
-    {
-        printf("I'm proc %d out of %d\n", myrank, numprocs);
-    }
+    printf("Hi, I am proc %d\n", myrank);
+  } else {
+    printf("I'm proc %d out of %d\n", myrank, numprocs);
+  }
 
-    if (myrank == 0)
-    {
-        print_X();
-    }
+  if (myrank == 0) {
+    print_X();
+  }
 
-    MPI_Finalize();
+  MPI_Finalize();
 
-    exit(0);
+  exit(0);
 }
 
 /* ------------------ Above Was Provided --------------------- */
@@ -177,40 +153,33 @@ int main(int argc, char *argv[])
 /* Provided global variables are MAXN, N, A[][], B[], and X[],
  * defined in the beginning of this code.  X[] is initialized to zeros.
  */
-void source_gauss()
-{
-    int norm, row, col; /* Normalization row, and zeroing
-                         * element row and col */
-    float multiplier;
+void source_gauss() {
+  int norm, row, col; /* Normalization row, and zeroing
+                       * element row and col */
+  float multiplier;
 
-    printf("Computing Serially.\n");
+  printf("Computing Serially.\n");
 
-    /* Gaussian elimination */
-    for (norm = 0; norm < N - 1; norm++)
-    {
-        for (row = norm + 1; row < N; row++)
-        {
-            multiplier = A[row][norm] / A[norm][norm];
-            for (col = norm; col < N; col++)
-            {
-                A[row][col] -= A[norm][col] * multiplier;
-            }
-            B[row] -= B[norm] * multiplier;
-        }
+  /* Gaussian elimination */
+  for (norm = 0; norm < N - 1; norm++) {
+    for (row = norm + 1; row < N; row++) {
+      multiplier = A[row][norm] / A[norm][norm];
+      for (col = norm; col < N; col++) {
+        A[row][col] -= A[norm][col] * multiplier;
+      }
+      B[row] -= B[norm] * multiplier;
     }
-    /* (Diagonal elements are not normalized to 1.  This is treated in back
-     * substitution.)
-     */
+  }
+  /* (Diagonal elements are not normalized to 1.  This is treated in back
+   * substitution.)
+   */
 
-    /* Back substitution */
-    for (row = N - 1; row >= 0; row--)
-    {
-        X[row] = B[row];
-        for (col = N - 1; col > row; col--)
-        {
-            X[row] -= A[row][col] * X[col];
-        }
-        X[row] /= A[row][row];
+  /* Back substitution */
+  for (row = N - 1; row >= 0; row--) {
+    X[row] = B[row];
+    for (col = N - 1; col > row; col--) {
+      X[row] -= A[row][col] * X[col];
     }
+    X[row] /= A[row][row];
+  }
 }
-
